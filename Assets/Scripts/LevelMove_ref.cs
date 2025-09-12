53ac0bb51fa283060476debae645a94f0081f1c5
Xxshaym1nxX <hobbescomedian@gmail.com>
2025-09-12 00:13:43 -0400

    fixed a bug w/ level transport

--- Assets/Scripts/LevelMove_ref.cs
+++ Assets/Scripts/LevelMove_ref.cs
@@ -4,13 +4,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
  
-public class LevelMove_Ref : MonoBehaviour
+public class LevelMove_ref : MonoBehaviour
 {
   
     private void OnTriggerEnter2D(Collider2D collision)
     {
        
         if(collision.tag == "Player") {
+            Debug.Log($"It works");
         SceneManager.LoadScene("Room_2");
         }
     }
